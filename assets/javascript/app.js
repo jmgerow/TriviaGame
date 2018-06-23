989bcce9a3da1fa264fcb01895f75e510de92dd8
jmgerow <justingerow1@gmail.com>
2018-06-22 21:14:01 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// need to have page open with start button.  document.ready?
+// need to enable on click for start button
+// need to display first question with timer, question, and 4 possible answers. 
+// need to show if answer was correct or not by writing to html
+// need to automatically go to next question
+// when all questions are completed, show score
+// add reset button to score page to reset game
